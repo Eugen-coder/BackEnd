9b1038919c58e4afbddb75e8512c92c14e3ce91a
Eugen-coder <eskhmelev@gmail.com>
2025-06-10 18:10:27 +0200

    HW20250610

---
+++ ElevatorAppl.java
@@ -0,0 +1,33 @@
+package ait.elevator;
+
+import ait.elevator.task.Elevator;
+import ait.elevator.task.Truck;
+
+public class ElevatorAppl {
+    private static final int N_TRUCK = 10_000;
+    private static final int N_RACES = 10;
+    private static final int CAPACITY = 20;
+
+    public static void main(String[] args) throws InterruptedException {
+        Elevator elevator1 = new Elevator("V. I. Lenin");
+        Elevator elevator2 = new Elevator("Karl Marx");
+
+        Truck[] trucks = new Truck[N_TRUCK];
+        for (int i = 0; i < trucks.length; i++) {
+            trucks[i] = new Truck(N_RACES, CAPACITY, elevator1, elevator2);
+        }
+
+        Thread[] threads = new Thread[trucks.length];
+        for (int i = 0; i < threads.length; i++) {
+            threads[i] = new Thread(trucks[i]);
+            threads[i].start();
+        }
+
+        for (int i = 0; i < threads.length; i++) {
+            threads[i].join();
+        }
+
+        System.out.println("Elevator " + elevator1.getName() + " has " + elevator1.getCurrentVolume());
+        System.out.println("Elevator " + elevator2.getName() + " has " + elevator2.getCurrentVolume());
+    }
+}
